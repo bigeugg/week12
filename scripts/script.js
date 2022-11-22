18a9a3e10e80835a1552abadfdacf8d888590687
Eugene Desktop <elee331@my.bcit.ca>
2022-11-22 15:22:56 -0800

    changed background colour to pink

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,12 @@ function clickHandler()
 clickButton.addEventListener("click", clickHandler, {once:true}); //gives it to us once
 
 
+
+const pink = document.querySelector(".body");
+
+function changeBGPink()
+{
+    // document.body.style.backgroundColor = `pink` //this way was done in quiz: one way
+    document.body.classList.add("pinkBG");
+}
+clickButton.addEventListener('click', changeBGPink)
